f306e05923eb1a907eec7d9ab3ad2e305661fd92
Aman Ansari <100956289+Amann09@users.noreply.github.com>
2024-11-12 17:24:02 +0530

    Create shaded_region_bet_two_circles.py

---
+++ shaded_region_bet_two_circles.py
@@ -0,0 +1,15 @@
+import math
+
+def area_of_sector(angle, radius): # angle (Celcius)
+    area = (angle/360) * math.pi * (radius**2)
+    return area
+    
+def area_of_triangle(angle, radius):
+    area = (1/2) * (radius**2) * math.sin(angle)
+    return area
+
+def area_of_shaded_region(a1, r1, a2, r2):
+    area = (area_of_sector(a1, r1) - area_of_triangle(a1, r1)) + (area_of_sector(a2, r2) - area_of_triangle(a2, r2))
+    return area
+    
+print(area_of_shaded_region(30, 4, 45, 4))
